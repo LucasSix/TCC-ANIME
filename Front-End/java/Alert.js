da5cc8f74e9a2fe61f3606ea0e8cb0d799575df0
LucasSix <victorsantosr2002@gmail.com>
2020-10-27 09:58:04 -0300

    Tcc anime

---
+++ Front-End/java/Alert.js
@@ -0,0 +1,8 @@
+<Html lang="pt-br">
+    <head>
+        <meta charset="utf-8"></meta>
+         <title> Notificação</title>
+         <link rel="stylesheet" href="../pages/logo2.css"
+         
+    </head>
+</Html>
